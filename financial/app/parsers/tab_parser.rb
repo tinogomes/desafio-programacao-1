8b27b6c3cf43f9e34a1723660721c228c22d0129
tinogomes <tinorj@gmail.com>
2013-07-05 07:28:12 -0300

    criado parser para arquivo csv separado por tab

---
+++ financial/app/parsers/tab_parser.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+class TabParser
+  attr_reader :file, :results
+
+  def initialize(file)
+    @file = file
+    @results = []
+  end
+
+  def process!
+    require 'csv'
+    @results = CSV.parse(file, col_sep: "\t", headers: :first_row).map do |row|
+      normalize(row.to_hash)
+    end
+  end
+  
+  def normalize(hash)
+    {}.tap do |resulting|
+      hash.map {|key, value| resulting[slugify(key)] = value }
+    end
+  end
+
+  def slugify(string)
+    string.tr(' ', '_')
+  end
+end
---
+++ financial/spec/parsers/tab_parser_spec.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+require_relative '../../app/parsers/tab_parser'
+
+describe TabParser do
+  let(:file) { File.open('./spec/fixtures/example_input.tab') }
+
+  subject { TabParser.new(file) }
+  
+  before { subject.process! }
+
+  it 'should process csv tab file to array hashes' do
+    expect(subject.results.count).to eql(4)
+  end
+  
+  describe 'checking first row' do
+    let(:first_row) { subject.results.first }
+    
+    it 'should be equal to expected hash' do
+      expect(first_row).to eql({
+        'item_description' => 'R$10 off R$20 of food',
+        'item_price' => '10.0',
+        'merchant_address' => '987 Fake St',
+        'merchant_name' => %Q{Bob's Pizza},
+        'purchase_count' => '2',
+        'purchaser_name' => 'João Silva'
+      })
+    end
+  end
+end
