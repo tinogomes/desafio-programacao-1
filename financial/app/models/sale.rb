7836ac6c0e464f373add40c546723be9483bb831
tinogomes <tinorj@gmail.com>
2013-07-05 22:43:12 -0300

    testando modelo de vendas

--- financial/app/models/sale.rb
+++ financial/app/models/sale.rb
@@ -1,16 +1,24 @@
 # encoding: utf-8
 
 class Sale < ActiveRecord::Base
-  validates :purchaser_name, :item_description, :item_price,
-            :merchant_address, :merchant_name,
-            presence: true
+  REQUIRED_ATTRIBUTES = [
+    :purchaser_name,
+    :item_description,
+    :item_price,
+    :merchant_address,
+    :merchant_name,
+    :purchase_count,
+    :transaction_key
+    ]
+
+  validates(*REQUIRED_ATTRIBUTES, presence: true)
 
   validates :purchase_count,
-            presence: true,
-            numerally: {
-              greater_than_or_equal_to: 0,
+            numericality: {
+              allow_nil: true,
+              greater_than_or_equal_to: 1,
               only_integer: true
             }
 
-  validates :transaction_key, presence: true, uniqueness: true
+  validates :transaction_key, uniqueness: true
 end
