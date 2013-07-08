66c461a48b0b2b2fa05183ddac06327d077a3b35
tinogomes <tinorj@gmail.com>
2013-07-08 17:49:27 -0300

    pagina para ping

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
   def index
     redirect_to sales_path
   end
+  
+  def ping
+    render text: 'pong'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Financial::Application.routes.draw do
   resources :sales, except: [:edit, :update, :destroy]
 
+  get 'ping', to: 'home#ping'
   root to: 'home#index'
 end
